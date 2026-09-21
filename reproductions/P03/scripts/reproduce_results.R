de0ab243a52baa5f695d1445b978e29cf5c04481
amachn <amachann2005@gmail.com>
2026-09-21 15:04:42 -0500

    feat(P03): add permutation code
    currently untested and needs more documentation

--- reproductions/P03/scripts/reproduce_results.R
+++ reproductions/P03/scripts/reproduce_results.R
@@ -17,6 +17,53 @@ chromatin_states <- import.bed(chromatin_file)
 nads <- import.bed(nad_file)
 lads <- import.bed(lad_file)
 
-# hg38 gene annotations
+# construct the canonical hg38 genome (chr1-22, chrX, chrY)
+human_canon <- getGenomeAndMask("hg38", mask = NA)$genome |>
+  filterChromosomes(chr.type = "canonical", organism = "hg")
+
+# create hg38 gene annotations and restrict to canonical genes
 txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
-genes_hg38 <- genes(txdb)
+all_genes <- genes(txdb)
+canonical_genes <- all_genes[seqnames(all_genes) %in% seqnames(human_canon)]
+
+# define targets
+chromatin_state_names <- unique(chromatin_states$name)
+
+targets <- list(
+  Genes = canonical_genes,
+  NADs  = nads,
+  LADs  = lads
+)
+
+results <- data.frame(
+  chromatin_state = character(),
+  target = character(),
+  p_value = numeric(),
+  z_score = numeric()
+)
+
+# permutations
+for (state_name in chromatin_state_names) {
+  state_regions <- subset(chromatin_states, name == state_name)
+  
+  for (target_name in names(targets)) {
+    test <- permTest(
+      A = state_regions, B = targets[[target_name]], ntimes = 100,
+      genome = human_canon, universe = chromatin_states,
+      randomize.function = resampleRegions, evaluate.function = numOverlaps,
+      per.chromosome = FALSE, verbose = TRUE
+    )
+    
+    test_summary <- summary(test)
+    
+    results <- rbind(
+      results,
+      data.frame(
+        chromatin_state = state_name,
+        target = target_name,
+        p_value = test_summary$pvalue,
+        z_score = test_summary$zscore
+      )
+    )
+  }
+}
