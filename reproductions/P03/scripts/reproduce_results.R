3ba1825c9bc90850d93e40f4d109fd46ea105118
amachn <amachann2005@gmail.com>
2026-09-21 14:36:36 -0500

    feat(P03): base data loading

--- reproductions/P03/scripts/reproduce_results.R
+++ reproductions/P03/scripts/reproduce_results.R
@@ -1,3 +1,22 @@
 # ~~~ reproductions/P03/scripts/reproduce_results.R ~~~
 # - reproduce Chan et al. 2022 figure 5-4c genomic annotation enrichment
 # * make sure your CWD is set to .../null-model-sensitivity/reproductions/P03
+library(rtracklayer)
+library(regioneR)
+library(TxDb.Hsapiens.UCSC.hg38.knownGene)
+
+data_dir <- "data"
+
+# define file paths
+chromatin_file <- file.path(data_dir, "hg38_im90_chromatin_states.bed")
+nad_file <- file.path(data_dir, "hg38_im90_nads.bed")
+lad_file <- file.path(data_dir, "hg38_im90_lads.bed")
+
+# load genomic intervals
+chromatin_states <- import.bed(chromatin_file)
+nads <- import.bed(nad_file)
+lads <- import.bed(lad_file)
+
+# hg38 gene annotations
+txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
+genes_hg38 <- genes(txdb)
